3041f8225875a5321be15f61e007853ae5312278
Ben Johnson <monk.sound.design@gmail.com>
2023-07-12 03:43:39 -0400

    set form values into redux state

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,44 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
 import { inputChange, postQuiz } from "../state/action-creators";
-import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export function Form(props) {
-  const [question, setQuestion] = useLocalStorage(
-    "question",
-    props.form.newQuestion
-  );
-  const [trueAnswer, setTrueAnswer] = useLocalStorage(
-    "trueAnswer",
-    props.form.newTrueAnswer
-  );
-  const [falseAnswer, setFalseAnswer] = useLocalStorage(
-    "falseAnswer",
-    props.form.newFalseAnswer
-  );
   const { inputChange, postQuiz } = props;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    inputChange({
+      ...props.form,
+      [name]: value,
+    });
+  };
 
   const onSubmit = (evt) => {
     const newQuizQuestion = {
-      newQuestion: question,
-      newTrueAnswer: trueAnswer,
-      newFalseAnswer: falseAnswer,
+      newQuestion: props.form.newQuestion,
+      newTrueAnswer: props.form.newTrueAnswer,
+      newFalseAnswer: props.form.newFalseAnswer,
     };
 
     const newQuizPost = {
-      question_text: question,
-      true_answer_text: trueAnswer,
-      false_answer_text: falseAnswer,
+      question_text: props.form.newQuestion,
+      true_answer_text: props.form.newTrueAnswer,
+      false_answer_text: props.form.newFalseAnswer,
     };
 
-    const questionMessageText = question;
+    const questionMessageText = props.form.newQuestion;
 
     evt.preventDefault();
     inputChange(newQuizQuestion);
     postQuiz(newQuizPost, questionMessageText);
-    setQuestion("");
-    setTrueAnswer("");
-    setFalseAnswer("");
   };
 
   return (
@@ -46,31 +39,34 @@ export function Form(props) {
       <h2>Create New Quiz</h2>
       <input
         maxLength={50}
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
+        value={props.form.newQuestion}
+        name="newQuestion"
+        onChange={handleChange}
         id="newQuestion"
         placeholder="Enter question"
       />
       <input
         maxLength={50}
-        value={trueAnswer}
-        onChange={(e) => setTrueAnswer(e.target.value)}
+        value={props.form.newTrueAnswer}
+        name="newTrueAnswer"
+        onChange={handleChange}
         id="newTrueAnswer"
         placeholder="Enter true answer"
       />
       <input
         maxLength={50}
-        value={falseAnswer}
-        onChange={(e) => setFalseAnswer(e.target.value)}
+        value={props.form.newFalseAnswer}
+        name="newFalseAnswer"
+        onChange={handleChange}
         id="newFalseAnswer"
         placeholder="Enter false answer"
       />
       <button
         id="submitNewQuizBtn"
         disabled={
-          question.trim().length >= 2 &&
-          trueAnswer.trim().length >= 2 &&
-          falseAnswer.trim().length >= 2
+          props.form.newQuestion.trim().length >= 2 &&
+          props.form.newTrueAnswer.trim().length >= 2 &&
+          props.form.newFalseAnswer.trim().length >= 2
             ? false
             : true
         }
